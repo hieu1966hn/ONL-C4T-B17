aeb658087316ece59b83295d997089aefd7bb091
hieu1966hn <hieu1966hn@gmail.com>
2020-12-05 23:33:38 +0700

    new update code 05 - 12- 2020

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-// let user = {
-//     name: "hung",
-//     age: 18,
-//     address: '...'
-// }
-// console.log(user.address);
+let users = [];
+const form_info = document.querySelector(".signup-form");
+
+const error = (somthing) => {
+    return document.querySelector('.' + somthing);
+}
+
+console.log(form_info);
+form_info.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const user = {
+        name: form_info.name.value,
+        email: form_info.email.value,
+        password: form_info.password.value
+    }
+    fixing_form(user);
+});
