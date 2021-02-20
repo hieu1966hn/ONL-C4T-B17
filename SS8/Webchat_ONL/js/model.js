3acf4b2494a0e63e50f12b3d0f5584f6d4e52f41
hieu1966hn <hieu1966hn@gmail.com>
2021-02-20 21:53:17 +0700

    new code today, fix model

--- SS8/Webchat_ONL/js/model.js
+++ SS8/Webchat_ONL/js/model.js
@@ -1,7 +1,10 @@
 const model ={};
 model.register = (data)=>{ // hàm nhận vào 1 data
     firebase.auth()
-    .createUserWithEmailAndPassword(data.email, data.password); // truyền vào email và pass
+    .createUserWithEmailAndPassword(data.email, data.password) // truyền vào email và pass
+    .catch(err => {
+        console.log(err);
+    });
+    // catch để bắt lỗi 
 
-    
 }
